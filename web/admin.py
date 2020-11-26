432e9d715d75e64166d4aaa7070cb8edd38d5140
manouchehr <manoochehr.solamani@gmail.com>
2020-11-26 19:34:28 +0330

    add Network_destination Model

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Network_destination
+admin.site.register(Network_destination)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Network_destination(models.Model):
+    id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
+    name=models.CharField(max_length = 100)
+    path=models.CharField(max_length = 200)
+    user=models.CharField(max_length = 50, blank=True)
+    password=models.CharField(max_length = 50, blank=True   )
+    protocol = models.CharField(max_length=50)
