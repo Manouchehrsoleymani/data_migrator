f49bcfaefedb34ff716d808ae84d1e050ece50e8
manouchehr <manoochehr.solamani@gmail.com>
2020-11-26 19:50:09 +0330

    Source path Model Added

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Network_destination
+from .models import Network_destination, Source_path
 admin.site.register(Network_destination)
+admin.site.register(Source_path)
--- web/models.py
+++ web/models.py
@@ -8,3 +8,9 @@ class Network_destination(models.Model):
     user=models.CharField(max_length = 50, blank=True)
     password=models.CharField(max_length = 50, blank=True   )
     protocol = models.CharField(max_length=50)
+
+class Source_path(models.Model):
+    id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
+    name=models.CharField(max_length = 100)
+    path=models.CharField(max_length = 255)
+    
