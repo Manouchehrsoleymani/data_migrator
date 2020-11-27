35547c94bb7703f91ad00389d8cf47ce282071c9
root <root@localhost.localdomain>
2020-11-27 18:25:08 +0330

    policy & Policy process Models were added

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Network_destination, Source_path
+from .models import Network_destination, Source_path,Policy,Policy_process
 admin.site.register(Network_destination)
 admin.site.register(Source_path)
+admin.site.register(Policy)
+admin.site.register(Policy_process)
--- web/models.py
+++ web/models.py
@@ -13,4 +13,18 @@ class Source_path(models.Model):
     id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
     name=models.CharField(max_length = 100)
     path=models.CharField(max_length = 255)
+
+class Policy(models.Model):
+    id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
+    name=models.CharField(max_length = 100)
+    operation=models.CharField(max_length = 255)
+    regex=models.CharField(max_length = 255)
+
+class Policy_process(models.Model):
+    id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
+    name=models.CharField(max_length = 100)
+    proirity=models.BigIntegerField()
+    source_id = models.BigIntegerField()
+    dest_id = models.BigIntegerField()
+    policy_id = models.BigIntegerField()
     
