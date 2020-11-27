72936190332677525d37e34a27d090bae6b42722
manouchehr <manoochehr.solamani@gmail.com>
2020-11-27 19:05:44 +0330

    add some code to return human readable text in Web UI for each Models

--- web/models.py
+++ web/models.py
@@ -8,17 +8,23 @@ class Network_destination(models.Model):
     user=models.CharField(max_length = 50, blank=True)
     password=models.CharField(max_length = 50, blank=True   )
     protocol = models.CharField(max_length=50)
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.path)
 
 class Source_path(models.Model):
     id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
     name=models.CharField(max_length = 100)
     path=models.CharField(max_length = 255)
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.path)
 
 class Policy(models.Model):
     id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
     name=models.CharField(max_length = 100)
     operation=models.CharField(max_length = 255)
     regex=models.CharField(max_length = 255)
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.operation)
 
 class Policy_process(models.Model):
     id=models.AutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='ID')
@@ -27,4 +33,5 @@ class Policy_process(models.Model):
     source_id = models.BigIntegerField()
     dest_id = models.BigIntegerField()
     policy_id = models.BigIntegerField()
-    
+    def __str__(self):
+        return self.name
